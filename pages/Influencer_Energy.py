26360ccad3bc11559a34664a30a28cd25a8c5dd4
loyidandrews <54929064+loyidandrews@users.noreply.github.com>
2022-11-16 10:52:14 +0100

    Add files via upload

---
+++ pages/Influencer_Energy.py
@@ -0,0 +1,10 @@
+import streamlit as st
+def app():
+    st.title('APP1')
+    st.write('Welcome to app1')
+
+st.image(
+    "https://www.pngkey.com/png/detail/441-4412134_page-under-construction-under-construction-sign-png.png",
+    width=400,
+     )
+st.subheader('Ooops.. App under Construction, Come back later')
