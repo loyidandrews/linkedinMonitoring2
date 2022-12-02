1e3ca07ab64fe16b8700cc43aafb242b5c6d1898
loyidandrews <54929064+loyidandrews@users.noreply.github.com>
2022-12-02 11:50:48 +0100

    Update Dobner_LinkedIn_Keyword_Monitor.py

--- pages/Dobner_LinkedIn_Keyword_Monitor.py
+++ pages/Dobner_LinkedIn_Keyword_Monitor.py
@@ -2,7 +2,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 #from st_aggrid import AgGrid
 
 #import pandas_profiling
